d29f4a2624b2f8e3be3591d5542c7b0cafd9c331
AOKanalyst <69393449+aokanalyst@users.noreply.github.com>
2020-11-23 14:22:11 -0500

    Update FirstClass.java
    Making new arrangement

--- src/git_project004/FirstClass.java
+++ src/git_project004/FirstClass.java
@@ -4,22 +4,16 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 
-
       System.out.println("This is the first commit changes from local master");
       System.out.println("This is the third commit from local master");
 	System.out.println("This is the first commit from the remote master");
 	System.out.println("This is the fourth commit from local master");
-	
-	
-	
-	
-
+		
       System.out.println("This is the first changes from local master");
       System.out.println("This is the second changes from local master");
       System.out.println("This is the third changes from local master");
 	System.out.println("This is the first changes from the remote master");	
-		
-		
+				
 		System.out.println("This is the second commit from remote master");
 
 	}
