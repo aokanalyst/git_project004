277e81b11fc2189e5360999b698742eb2997e85b
AOKanalyst <aokanalyst@gmail.com>
2020-11-24 10:02:53 -0500

    the second change from ali local

--- src/git_project004/FirstClass.java
+++ src/git_project004/FirstClass.java
@@ -17,6 +17,8 @@ public class FirstClass {
 		System.out.println("This is the second commit from remote master");
 		
 		System.out.println("This is the first commit from ali master");
+		
+		System.out.println("This is the second commit from local ali");
 
 	}
 
